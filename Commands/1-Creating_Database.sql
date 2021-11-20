5e94fcdc4d7cc22682f5d305e139f59bbdc5e4d1
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2021-11-20 23:37:47 +0530

    SQL Commands

--- Commands/1-Creating_Database.sql
+++ Commands/1-Creating_Database.sql
@@ -1,4 +1,4 @@
--- SQL Syntax
+-- SQL Syntax 
 /*
 All the SQL statements start with any of the keywords like
 SELECT, INSERT, UPDATE, DELETE, ALTER, DROP, CREATE, USE, SHOW and all the statements end with a semicolon (;).
